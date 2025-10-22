46e4d1862685864789cde945dccf5097379464fb
Mohamed Boukerche <mohamedboukerche55@gmail.com>
2025-10-22 22:06:14 +0100

    Refactor train.py for model training and evaluation

--- train.py
+++ train.py
@@ -1,18 +1,15 @@
-# train_digit_model.py
 import tensorflow as tf
 from tensorflow.keras import layers, models
 import numpy as np
 
-# Load MNIST
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 x_train = x_train.astype("float32") / 255.0
 x_test  = x_test.astype("float32") / 255.0
 
-# Expand dims -> (N,28,28,1)
 x_train = np.expand_dims(x_train, -1)
 x_test  = np.expand_dims(x_test, -1)
 
-# Model
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation='relu', input_shape=(28,28,1)),
     layers.MaxPooling2D((2,2)),
@@ -30,13 +27,13 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-# Train
+
 model.fit(x_train, y_train, epochs=5, batch_size=128, validation_split=0.1)
 
-# Evaluate
+
 loss, acc = model.evaluate(x_test, y_test)
 print("Test accuracy:", acc)
 
-# Save
+
 model.save("digit_model.h5")
 print("Saved model to digit_model.h5")
